e1ae9079b411ea5bf48622175255d2f22e2e96ef
Ethan Sternke <ethan.sternke@gmail.com>
2018-10-03 14:14:28 -0400

    error handling for PUT + formatting/comments

--- app.py
+++ app.py
@@ -12,19 +12,32 @@ latest_id = 0
 
 class TodoResource(Resource):
     def get(self, id):
+
+        # return first id match in todos
         for t in todos:
             if t.get('id') == int(id):
                 return t
+
+        # return error if no match
         return {'error': 'No todo with id %s' % (id)}
 
     def put(self, id):
-        data = request.get_json()
+
         global todos
+        data = request.get_json()
         todo_index = 0
         current_time = time.time()
+
+        # find index of first id match
         for t in todos:
             if t.get('id') == int(id):
                 todo_index = todos.index(t)
+
+        # return error if no match
+        if todo_index == 0:
+            return {'error': 'No todo with id %s' % (id)}
+
+        # update with user data and new times
         todos[todo_index].update(data)
         todos[todo_index].update({
             'last_updated_at': current_time
@@ -33,13 +46,20 @@ class TodoResource(Resource):
             todos[todo_index].update({
                 'completed_at': current_time
             })
+
         return todos[todo_index]
 
     def delete(self, id):
+
         global todos
+
+        # make new todo list without the given id
         new_todos = [t for t in todos if t.get('id') != int(id)]
+
+        # return error if nothing was removed
         if len(new_todos) == len(todos):
             return {'error': 'No todo with id %s' % (id)}
+
         else:
             todos = new_todos
             return {'success': 'Deleted todo with id %s' % (id)}
